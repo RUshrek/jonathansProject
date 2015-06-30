cf2dcb9d18d6c7fd3a15f132302823112f22ab6c
Student 10AM <student10@js-c02mxpwcg083.punahou.edu>
2015-06-30 12:58:31 -1000

    123456789

---
+++ untitled.js
@@ -0,0 +1,8 @@
+Math.floor((Math.random()*10)*1);
+Math.random();
+Math.floor(Math.random());
+Math.ceil(Math.random());
+//math.random picks a random number between 1 and 0
+//math.floor(math.random()); gives you 0 because it is lowest possable between 1 and 0
+//math.ceil(math.random()); gives you 1 because it is the highest possable between 1 and 0
+//Math.floor((Math.random()*10)*1); give you a number between 10 and 1 or whatever you put in
