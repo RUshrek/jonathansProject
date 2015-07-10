5dc0f2753d45676ef8c77bed15aae2b217525787
zyamamoto20 <student10@js-c02mxpwcg083.punahou.edu>
2015-07-09 14:30:57 -1000

    ok

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-$ (confirm("Sophos Anti-Virus has found a virus on this page.") === true );
-	
+if( confirm("Sophos Anti-Virus has found a virus on this page. Sophos Anti-Virus WILL SCAN THIS PAGE.") === true ){
+	alert("SCANNING");
+	alert("SCAN COMPLETE");
+	}
+	 
 
 // python -m SimpleHTTPServer 8000
